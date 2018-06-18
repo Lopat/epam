990ff89af36e40df43987438136cef1788b12dad
Dmitry <euroshow.light.demyanov@gmail.com>
2018-06-18 12:35:33 +0300

    homework 4 - 3

--- src/main/java/com/ddemyanov/javase04/t03/CharsetChanger.java
+++ src/main/java/com/ddemyanov/javase04/t03/CharsetChanger.java
@@ -1,6 +1,7 @@
 package com.ddemyanov.javase04.t03;
 
-import java.io.FileReader;
+import java.io.*;
+import java.nio.file.Paths;
 
 /**
  * Дан файл, содержащий буквы текст на кириллице.
@@ -9,7 +10,30 @@ import java.io.FileReader;
  */
 public class CharsetChanger {
 
-    public static void main(String[] args) {
-//        FileReader reader = new FileReader("")
+    private static BufferedReader reader;
+    private static BufferedWriter writer;
+
+    static {
+        try {
+            System.out.println(Paths.get("src/main/resources/utf8.txt").toAbsolutePath());
+
+            reader = new BufferedReader(new FileReader("C:\\Users\\Дмитрий\\epam\\src\\main\\resources\\utf8.txt"));
+            writer = new BufferedWriter(new FileWriter("C:\\Users\\Дмитрий\\epam\\src\\main\\resources\\utf-16.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        String line;
+        while (null != (line = reader.readLine())) {
+            String buff = new String(line.getBytes("UTF-8"));
+            String toWrite = new String(buff.getBytes("UTF-16"));
+            writer.write(toWrite);
+        }
+
+        reader.close();
+        writer.close();
     }
 }
