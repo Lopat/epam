c458821e11a38511b4f8d1b18e0f894a264ba818
Dmitry <euroshow.light.demyanov@gmail.com>
2018-06-18 15:05:55 +0300

    homework 5 - 2 property reader

---
+++ src/main/java/com/ddemyanov/javase06/t01/CollectionsClass.java
@@ -0,0 +1,7 @@
+package com.ddemyanov.javase06.t01;
+
+/**
+ * Изучите классы реализации коллекций и заполните следующую таблицу
+ Class|Ordering|Random Access| Key-Value Pairs| Allows Duplicates| Allows Null Values| Thread Safe|Blocking Operations
+ */
+public class CollectionsClass {}
---
+++ src/main/java/com/ddemyanov/javase06/t04/CollectionsFunctions.java
@@ -0,0 +1,11 @@
+package com.ddemyanov.javase06.t04;
+
+/**
+ * Заполните таблицу.
+ * Interface | Основная функциональность | Примеры типичного использования
+ * Set
+ * List
+ * Queue
+ * Map
+ */
+public class CollectionsFunctions {}
