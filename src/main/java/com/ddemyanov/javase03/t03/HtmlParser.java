e6dbf23d83791811379055559e71bc8fba32c246
Dmitry <euroshow.light.demyanov@gmail.com>
2018-07-22 21:53:09 +0300

    homework 3 t03 done

--- src/main/java/com/ddemyanov/javase03/t03/HtmlParser.java
+++ src/main/java/com/ddemyanov/javase03/t03/HtmlParser.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -13,25 +14,33 @@ import java.util.regex.Pattern;
  * Статья приведена в приложении к данному файлу заданий.
  */
 
-
 public class HtmlParser {
 
-//    public static void main(String[] args) {
-//        try {
-//            URL url = new URL("https://ru.pinterest.com/");
-//            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-//            String line;
-//
-//            //<a href="sample.html"><img src="images/sample.gif" width="50" height="50" alt="Пример"></a>
-//            Pattern pattern = Pattern.compile("<[^>]+>");
-//
-//            while ((line = br.readLine()) != null) {
-//                System.out.println(line);
-//            }
-//
-//            br.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("не нашел приложенную к заданию страницу");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+
+
+            Pattern pattern = Pattern.compile(
+                    "<a[^>]+href[\\s]?=[\\s]?[\"']([\\w/]+.)(html|htm)[\"']\\s*[^>]*>" +
+                    "<img[^>]+src[\\s]?=[\\s]?[\"']([\\w/]+.)(gif|jpg|jpeg|png|bmp)[\"'][^>]*></a>");
+            //<a href="sample.html"><img src="images/sample.gif" width="50" height="50" alt="Пример"></a>
+
+            while ((line = br.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if(matcher.matches())
+                    System.out.println(matcher.group(1) + matcher.group(2) + " = " + matcher.group(3) + matcher.group(4));
+            }
+
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+//    String s = "<a href=\"sample.html\"><img src=\"images/sample.gif\" width=\"50\" height=\"50\" alt=\"Пример\"></a>";
+//    Matcher m = pattern.matcher(s);
+//     if (m.matches()) System.out.println(m.group(1) + m.group(2) + " = " + m.group(3) + m.group(4));
